8dce8ef2c8b03d58007166231c3d20972731a93e
PRASHANT PATIL <manjifera@gmail.com>
2022-08-20 20:11:05 +0530

    demo changes

--- Twocharacters.java
+++ Twocharacters.java
@@ -5,7 +5,7 @@ public class Twocharacters {
 	static int alternate(String s) {
 		//beabeefeab b=3, e=4, a=2, f=1
 		//abaacdabd = a=4, b=2, c=1, d=2  bdbd
-		
+		//this is demo change... 
 		return 1;
 	}
 	public static void main(String[] args) {
